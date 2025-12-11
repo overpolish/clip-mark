6db701a711516c7213e9196ae52747b4baf519f4
Dom <dom@overpolish.co>
2025-12-11 14:26:21 +0000

    Add Mantine

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@mantine/core";
+
 function App() {
   return (
     <main className="container">
       <h1>Welcome to Tauri + React</h1>
+      <Button>Hello Mantine Button</Button>
     </main>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { MantineProvider } from "@mantine/core";
+import "@mantine/core/styles.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <MantineProvider
+      defaultColorScheme="auto"
+      theme={{
+        activeClassName: "",
+        defaultRadius: "md",
+      }}
+    >
+      <App />
+    </MantineProvider>
   </React.StrictMode>
 );
