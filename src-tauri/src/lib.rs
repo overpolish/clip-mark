0d2d72e7d8a57b9968a81d6015970a5d556f297b
Dom <dom@overpolish.co>
2025-12-11 14:52:04 +0000

    Tray only window

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,4 +1,7 @@
-use tauri::tray::TrayIconBuilder;
+use tauri::{
+    tray::{TrayIconBuilder, TrayIconEvent},
+    Manager,
+};
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
@@ -7,7 +10,29 @@ pub fn run() {
         .setup(|app| {
             TrayIconBuilder::new()
                 .icon(app.default_window_icon().unwrap().clone())
+                .on_tray_icon_event(|tray, event| match event {
+                    TrayIconEvent::Click { .. } => {
+                        let app = tray.app_handle();
+                        let win = app.get_webview_window("clip-mark").unwrap();
+
+                        win.show().unwrap();
+                        win.set_focus().unwrap();
+                    }
+                    _ => {}
+                })
                 .build(app)?;
+
+            if let Some(win) = app.get_webview_window("clip-mark") {
+                let win_clone = win.clone();
+                win.on_window_event(move |event| {
+                    if let tauri::WindowEvent::Focused(focused) = event {
+                        if !focused {
+                            win_clone.hide().unwrap();
+                        }
+                    }
+                });
+            }
+
             Ok(())
         })
         .run(tauri::generate_context!())
