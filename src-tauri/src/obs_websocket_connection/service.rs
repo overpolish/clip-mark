1533bc1c3c5ee167e27e109e7bc7b9a3f6850470
Dom <dom@overpolish.co>
2025-12-13 07:03:41 +0000

    Refactor connector

--- src-tauri/src/obs_websocket_connection/service.rs
+++ src-tauri/src/obs_websocket_connection/service.rs
@@ -10,7 +10,6 @@ use crate::{
 
 pub async fn websocket_connection(app_handle: tauri::AppHandle) {
     let mut interval = tokio::time::interval(Duration::from_secs(1));
-
     let credentials_tx = app_handle.state::<CredentialsWatcher>();
     let mut credentials_rx = credentials_tx.credentials_tx.subscribe();
 
@@ -18,45 +17,19 @@ pub async fn websocket_connection(app_handle: tauri::AppHandle) {
         update_system_tray_icon(&app_handle, SystemTrayIcon::Default);
         interval.tick().await;
 
-        if let Some(obs_server_data) = get_obs_server_data(&app_handle) {
-            let client_connection = obws::Client::connect(
-                obs_server_data.address,
-                obs_server_data.port,
-                Some(obs_server_data.password),
-            )
-            .await;
+        let Some(obs_server_data) = get_obs_server_data(&app_handle) else {
+            continue;
+        };
 
-            if let Ok(client) = client_connection {
-                let version = client.general().version().await;
-                if let Ok(version) = version {
-                    println!("Connected to OBS Version {}", version.obs_version);
-                }
-
-                update_system_tray_icon(&app_handle, SystemTrayIcon::Connected);
-
-                if let Ok(events) = client.events() {
-                    futures::pin_mut!(events);
-
-                    loop {
-                        tokio::select! {
-                                Some(event) = events.next() => {
-                                    println!("{event:#?}");
-                                }
-                                _ = credentials_rx.changed() => {
-                                    println!("Credentials changed, reconnecting...");
-                                    drop(client);
-                                    break;
-                            }
-                        }
-                    }
-                }
-            } else {
-                println!(
-                    "Failed to connect to OBS WebSocket {:?}",
-                    client_connection.err()
-                );
+        let client = match connect_to_obs(obs_server_data).await {
+            Ok(client) => client,
+            Err(err) => {
+                println!("Failed to connect to OBS WebSocket: {:?}", err);
+                continue;
             }
-        }
+        };
+
+        handle_client_connection(&app_handle, client, &mut credentials_rx).await;
     }
 }
 
@@ -65,5 +38,47 @@ fn get_obs_server_data(app_handle: &tauri::AppHandle) -> Option<ObsServerData> {
         Some(lock.clone())
     } else {
         None
+    }
+}
+
+async fn connect_to_obs(
+    obs_server_data: ObsServerData,
+) -> Result<obws::Client, obws::error::Error> {
+    obws::Client::connect(
+        obs_server_data.address,
+        obs_server_data.port,
+        Some(obs_server_data.password),
+    )
+    .await
+}
+
+async fn handle_client_connection(
+    app_handle: &tauri::AppHandle,
+    client: obws::Client,
+    credentials_rx: &mut tokio::sync::watch::Receiver<()>,
+) {
+    if let Ok(version) = client.general().version().await {
+        println!("Connected to OBS Version {}", version.obs_version);
+    }
+
+    update_system_tray_icon(&app_handle, SystemTrayIcon::Connected);
+
+    let Ok(events) = client.events() else {
+        return;
+    };
+    futures::pin_mut!(events);
+
+    credentials_rx.borrow_and_update();
+
+    loop {
+        tokio::select! {
+                Some(event) = events.next() => {
+                        println!("{event:#?}");
+                }
+                _ = credentials_rx.changed() => {
+                        println!("Credentials changed, reconnecting...");
+                        break;
+                }
+        }
     }
 }
