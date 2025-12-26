f8fb7e0ef8274a97ff5fc2b9b75526bb540e0649
Dom <dom@overpolish.co>
2025-12-26 11:36:30 +0000

    Refactor typography

--- src/components/typography/kbd.tsx
+++ src/components/typography/kbd.tsx
@@ -1,54 +1,57 @@
-import React from "react";
+import { Fragment, type ComponentProps } from "react";
 
-import { cn } from "@/lib/utils";
+import { tv, type VariantProps } from "tailwind-variants";
 
-function Kbd({ className, ...props }: React.ComponentProps<"kbd">) {
-  return (
-    <kbd
-      data-slot="kbd"
-      className={cn(
-        `
-          pointer-events-none inline-flex h-5 w-fit min-w-5 items-center
-          justify-center gap-1 rounded-sm bg-foreground px-1 font-sans text-xs
-          font-medium text-background select-none
-        `,
-        "border-b-2 border-muted-foreground",
-        "[&_svg:not([class*='size-'])]:size-3",
-        `
-          in-data-[slot=tooltip-content]:bg-background/20
-          in-data-[slot=tooltip-content]:text-background
-          dark:in-data-[slot=tooltip-content]:bg-background/10
-        `,
-        className
-      )}
-      {...props}
-    />
-  );
+const kbdVariants = tv({
+  base: `
+    pointer-events-none inline-flex h-5 w-fit min-w-5 items-center
+    justify-center gap-1 rounded-sm border-b-2 border-muted-foreground
+    bg-foreground px-1 font-sans text-xs font-medium text-background select-none
+    in-data-[slot=tooltip-content]:bg-background/20
+    in-data-[slot=tooltip-content]:text-background
+    dark:in-data-[slot=tooltip-content]:bg-background/10
+    [&_svg:not([class*='size-'])]:size-3
+  `,
+});
+
+type KbdProps = ComponentProps<"kbd">;
+
+/**
+ * @public
+ */
+export function Kbd({ className, ...props }: KbdProps) {
+  const styles = kbdVariants({ className });
+  return <kbd className={styles} data-slot="kbd" {...props} />;
 }
 
-function KbdGroup({
+const kbdGroupVariants = tv({
+  base: "inline-flex items-center gap-1",
+});
+
+type KbdGroupProps = ComponentProps<"div"> &
+  VariantProps<typeof kbdGroupVariants> & {
+    keys?: string[];
+    withPlusSigns?: boolean;
+  };
+
+/**
+ * @public
+ */
+export function KbdGroup({
   className,
   keys,
   withPlusSigns,
   ...props
-}: React.ComponentProps<"div"> & {
-  keys?: string[];
-  withPlusSigns?: boolean;
-}) {
+}: KbdGroupProps) {
+  const styles = kbdGroupVariants({ className });
   return (
-    <kbd
-      className={cn("inline-flex items-center gap-1", className)}
-      data-slot="kbd-group"
-      {...props}
-    >
+    <kbd className={styles} data-slot="kbd-group" {...props}>
       {keys?.map((key, index) => (
-        <React.Fragment key={index}>
+        <Fragment key={index}>
           <Kbd>{key}</Kbd>
           {withPlusSigns && index < keys.length - 1 && <span>+</span>}
-        </React.Fragment>
+        </Fragment>
       ))}
     </kbd>
   );
 }
-
-export { Kbd, KbdGroup };
--- src/components/typography/label.tsx
+++ src/components/typography/label.tsx
@@ -1,26 +1,25 @@
+import { type ComponentProps } from "react";
+
 import * as LabelPrimitive from "@radix-ui/react-label";
+import { tv } from "tailwind-variants";
 
-import { cn } from "@/lib/utils";
+const labelVariants = tv({
+  base: `
+    flex items-center gap-2 text-sm leading-none font-medium select-none
+    group-data-[disabled=true]:pointer-events-none
+    group-data-[disabled=true]:opacity-50
+    peer-disabled:cursor-not-allowed peer-disabled:opacity-50
+  `,
+});
 
-function Label({
-  className,
-  ...props
-}: React.ComponentProps<typeof LabelPrimitive.Root>) {
+type LabelProps = ComponentProps<typeof LabelPrimitive.Root>;
+
+/**
+ * @public
+ */
+export function Label({ className, ...props }: LabelProps) {
+  const styles = labelVariants({ className });
   return (
-    <LabelPrimitive.Root
-      data-slot="label"
-      className={cn(
-        `
-          flex items-center gap-2 text-sm leading-none font-medium select-none
-          group-data-[disabled=true]:pointer-events-none
-          group-data-[disabled=true]:opacity-50
-          peer-disabled:cursor-not-allowed peer-disabled:opacity-50
-        `,
-        className
-      )}
-      {...props}
-    />
+    <LabelPrimitive.Root className={styles} data-slot="label" {...props} />
   );
 }
-
-export { Label };
